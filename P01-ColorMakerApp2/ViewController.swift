90f7984be1bff060bd8f0373776ffc2dc180b457
ramon baeza yepez <By_ramon@outlook.com>
2020-01-30 16:45:21 -0600

    Initial Commit

---
+++ P01-ColorMakerApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P01-ColorMakerApp2
+//
+//  Created by ByRamon on 1/30/20.
+//  Copyright © 2020 ByRamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
